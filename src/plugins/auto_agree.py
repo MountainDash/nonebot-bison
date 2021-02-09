771d44860b358f1851f5dd9e0ceb666cfad45eb3
felinae98 <731499577@qq.com>
2021-02-09 12:35:49 +0800

    update nonebot, fix autoagree

--- src/plugins/auto_agree.py
+++ src/plugins/auto_agree.py
@@ -1,4 +1,4 @@
-from nonebot import on_request
+from nonebot import on_request, logger
 from nonebot.adapters.cqhttp import Bot, Event
 from nonebot.permission import SUPERUSER
 from nonebot.typing import T_State
@@ -10,9 +10,11 @@ friend_req = on_request(priority=5)
 @friend_req.handle()
 async def add_superuser(bot: Bot, event: FriendRequestEvent, state: T_State):
     if event.user_id in bot.config.superusers:
-        await bot.set_friend_add_request(flag=event.id, approve=True)
+        await event.approve(bot)
+        logger.info('add user {}'.format(event.user_id))
 
 @friend_req.handle()
 async def agree_to_group(bot: Bot, event: GroupRequestEvent, state: T_State):
     if event.sub_type == 'invite' and event.user_id in bot.config.superusers:
-        await bot.set_group_add_request(flag=event.id, sub_type='invite', approve=True)
+        await event.approve(bot)
+        logger.info('add group {}'.format(event.group_id))

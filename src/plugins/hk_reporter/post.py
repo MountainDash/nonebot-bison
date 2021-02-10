cac1bb6ef8bd5637751e5db51ebea49be6e86cb2
felinae98 <731499577@qq.com>
2021-02-10 19:48:12 +0800

    update post

--- src/plugins/hk_reporter/post.py
+++ src/plugins/hk_reporter/post.py
@@ -1,12 +1,14 @@
+from dataclasses import dataclass, field
 from . import plugin_config
 from .utils import parse_text
 
+@dataclass
 class Post:
 
     target_type: str
     text: str
     url: str
-    pics: list[str]
+    pics: list[str] = field(default_factory=list)
 
     async def generate_messages(self):
         if plugin_config.hk_reporter_use_pic:
@@ -21,12 +23,6 @@ class Post:
         for pic in self.pics:
             res.append("[CQ:image,file={url}]".format(url=pic))
         return res
-
-    def __init__(self, target_type, text, url, pics=[]):
-        self.target_type = target_type
-        self.text = text
-        self.url = url
-        self.pics = pics
 
     def __str__(self):
         return 'type: {}\ntext: {}\nurl: {}\npic: {}'.format(self.target_type, self.text[:50], self.url, ','.join(map(lambda x: 'b64img' if x.startswith('base64') else x, self.pics)))

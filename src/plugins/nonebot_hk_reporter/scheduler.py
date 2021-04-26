337dc2e5fdc9f64ab856246cf48910fc1993b827
felinae98 <731499577@qq.com>
2021-04-26 17:35:21 +0800

    remove scheduler plugin

--- src/plugins/nonebot_hk_reporter/scheduler.py
+++ src/plugins/nonebot_hk_reporter/scheduler.py
@@ -1,9 +1,15 @@
-from nonebot import require
+from nonebot import require, get_driver
 from .send import do_send_msgs
 from .platform.utils import fetch_and_send
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-scheduler: AsyncIOScheduler = require('nonebot_plugin_apscheduler').scheduler
+scheduler = AsyncIOScheduler()
+
+async def _start():
+    scheduler.configure({"apscheduler.timezone": "Asia/Shanghai"})
+    scheduler.start()
+
+get_driver().on_startup(_start)
 
 @scheduler.scheduled_job('interval', seconds=10)
 async def weibo_check():
@@ -24,5 +30,5 @@ async def rss_check():
 #     await fetch_and_send('wechat')
 
 @scheduler.scheduled_job('interval', seconds=1)
-async def _():
+async def _send_msgs():
     await do_send_msgs()

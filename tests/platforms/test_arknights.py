cf6ad6bc8229975db483db554888e77cfe85969f
felinae98 <731499577@qq.com>
2021-07-29 21:35:33 +0800

    update test

--- tests/platforms/test_arknights.py
+++ tests/platforms/test_arknights.py
@@ -26,7 +26,7 @@ def arknights_list_1():
 @pytest.mark.asyncio
 @respx.mock
 async def test_fetch_new(arknights, dummy_user_subinfo, arknights_list_0, arknights_list_1):
-    ak_list_router = respx.get("http://ak-fs.hypergryph.com/announce/IOS/announcement.meta.json")
+    ak_list_router = respx.get("https://ak-conf.hypergryph.com/config/prod/announce_meta/IOS/announcement.meta.json")
     detail_router = respx.get("https://ak-fs.hypergryph.com/announce/IOS/announcement/675.html")
     ak_list_router.mock(return_value=Response(200, json=arknights_list_0))
     detail_router.mock(return_value=Response(200, text=get_file('arknights-detail-675.html')))

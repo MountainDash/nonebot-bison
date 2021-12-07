a7d8e4ec0c7ede2481f89d60d8f3b9c47450e0f1
felinae98 <731499577@qq.com>
2021-12-07 22:23:29 +0800

    update doc

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  title: 'Nonebot HK Reporter',
-  description: 'Docs for Nonebot HK Reporter',
+  title: 'Nonebot Bison',
+  description: 'Docs for Nonebot Bison',
   themeConfig: {
     nav: [
       { text: '主页', link: '/' },
       { text: '部署与使用', link: '/usage/' },
       { text: '开发', link: '/dev/' },
-      { text: 'Github', link: 'https://github.com/felinae98/nonebot-hk-reporter' }
+      { text: 'Github', link: 'https://github.com/felinae98/nonebot-bison' }
     ]
   }
 }

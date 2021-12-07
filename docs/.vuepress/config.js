ec4849fe5b3c3a68e655d622a8dfdaae489e5ea5
felinae98 <731499577@qq.com>
2021-12-07 21:58:00 +0800

    add docs back

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'Nonebot HK Reporter',
+  description: 'Docs for Nonebot HK Reporter',
+  themeConfig: {
+    nav: [
+      { text: '主页', link: '/' },
+      { text: '部署与使用', link: '/usage/' },
+      { text: '开发', link: '/dev/' },
+      { text: 'Github', link: 'https://github.com/felinae98/nonebot-hk-reporter' }
+    ]
+  }
+}

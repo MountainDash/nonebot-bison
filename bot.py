a9e8ad05d748906948a7a4458ba695c39032b202
felinae98 <731499577@qq.com>
2022-02-10 00:38:48 +0800

    update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 import nonebot
-from nonebot.adapters.cqhttp import Bot as CQHTTPBot
+from nonebot.adapters.onebot.v11 import Adapter as OneBotV11Adapter
 
 nonebot.init(command_start=[""])
 app = nonebot.get_asgi()
 
 driver = nonebot.get_driver()
-driver.register_adapter("cqhttp", CQHTTPBot)
+driver.register_adapter(OneBotV11Adapter)
 
-nonebot.load_builtin_plugins()
+nonebot.load_builtin_plugins("echo")
 nonebot.load_plugin("nonebot_plugin_help")
 nonebot.load_plugins("src/plugins")
 

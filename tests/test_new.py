520451ab3677dddd88ec012e7294264f8d834e55
felinae98 <731499577@qq.com>
2022-02-09 19:20:31 +0800

    add new test

---
+++ tests/test_new.py
@@ -0,0 +1,13 @@
+import pytest
+
+def test_add(n_plugin_module):
+    from nonebot_bison.config import Config
+    config = Config()
+    config.add_subscribe(
+            user='123',
+            user_type='group',
+            target='weibo_id',
+            target_name='weibo_name',
+            target_type='weibo',
+            cats=[],
+            tags=[])

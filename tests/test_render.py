af8cbf6ecf1317a556b3042063a3cb5d7341f01f
felinae98 <731499577@qq.com>
2022-02-24 15:26:51 +0800

    remove ipdb

--- tests/test_render.py
+++ tests/test_render.py
@@ -30,9 +30,6 @@ async def test_arknights(app: App):
     from nonebot_bison.platform.arknights import Arknights
 
     ak = Arknights()
-    import ipdb
-
-    ipdb.set_trace()
     res = await ak.parse(
         {"webUrl": "https://ak.hycdn.cn/announce/IOS/announcement/854_1644580545.html"}
     )

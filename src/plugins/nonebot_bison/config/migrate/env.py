0bf530035c8c512cbc68e9409946e7f443418a1f
felinae98 <731499577@qq.com>
2022-03-20 00:11:32 +0800

    update

--- src/plugins/nonebot_bison/config/migrate/env.py
+++ src/plugins/nonebot_bison/config/migrate/env.py
@@ -9,13 +9,15 @@ config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-fileConfig(config.config_file_name)
+fileConfig(config.config_file_name)  # type:ignore
 
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from ..db_model import Base
+
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:

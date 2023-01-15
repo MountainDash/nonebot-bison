087ed7314cf0330b4d0f696ca9c92b7646f55365
hemengyang <hmy0119@gmail.com>
2023-01-15 20:56:46 +0800

    重命名 alembic_version 表之前先检查是否存在且 version_num 属于插件

--- src/plugins/nonebot_bison/bootstrap.py
+++ src/plugins/nonebot_bison/bootstrap.py
@@ -1,7 +1,6 @@
 from nonebot.log import logger
-from nonebot_plugin_datastore import create_session
-from nonebot_plugin_datastore.db import post_db_init, pre_db_init
-from sqlmodel import text
+from nonebot_plugin_datastore.db import get_engine, post_db_init, pre_db_init
+from sqlalchemy import inspect, text
 
 from .config.config_legacy import start_up as legacy_db_startup
 from .config.db import data_migrate
@@ -11,14 +10,35 @@ from .scheduler.manager import init_scheduler
 
 @pre_db_init
 async def pre():
-    async with create_session() as session:
-        t = text(
-            """ALTER TABLE alembic_version RENAME TO nonebot_bison_alembic_version;"""
+    def _has_table(conn, table_name):
+        insp = inspect(conn)
+        return insp.has_table(table_name)
+
+    async with get_engine().begin() as conn:
+        if not await conn.run_sync(_has_table, "alembic_version"):
+            logger.debug("未发现默认版本数据库，开始初始化")
+            return
+
+        logger.debug("发现默认版本数据库，开始检查版本")
+        t = await conn.scalar(text("select version_num from alembic_version"))
+        if t not in [
+            "4a46ba54a3f3",  # alter_type
+            "5f3370328e44",  # add_time_weight_table
+            "0571870f5222",  # init_db
+            "a333d6224193",  # add_last_scheduled_time
+            "c97c445e2bdb",  # add_constraint
+        ]:
+            logger.warning(f"当前数据库版本：{t}，不是插件的版本，已跳过。")
+            return
+
+        logger.debug(f"当前数据库版本：{t}，是插件的版本，开始迁移。")
+        # 删除可能存在的版本数据库
+        if await conn.run_sync(_has_table, "nonebot_bison_alembic_version"):
+            await conn.execute(text("drop table nonebot_bison_alembic_version"))
+
+        await conn.execute(
+            text("alter table alembic_version rename to nonebot_bison_alembic_version")
         )
-        try:
-            await session.execute(t)
-        except:
-            pass
 
 
 @post_db_init

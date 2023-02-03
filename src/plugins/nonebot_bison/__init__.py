04f58d6a06b1e7f36269c596bc1e23b7e5d8dbff
uy/sun <hmy0119@gmail.com>
2023-02-03 21:15:01 +0800

    :sparkles: 适配 Nonebot 插件元数据 (#184)

--- src/plugins/nonebot_bison/__init__.py
+++ src/plugins/nonebot_bison/__init__.py
@@ -1,4 +1,4 @@
-from nonebot.plugin import require
+from nonebot.plugin import PluginMetadata, require
 
 require("nonebot_plugin_datastore")
 
@@ -19,6 +19,15 @@ from .plugin_config import plugin_config
 __help__version__ = "0.6.3"
 __help__plugin__name__ = "nonebot_bison"
 __usage__ = f"本bot可以提供b站、微博等社交媒体的消息订阅，详情请查看本bot文档，或者{'at本bot' if plugin_config.bison_to_me else '' }发送“添加订阅”订阅第一个帐号，发送“查询订阅”或“删除订阅”管理订阅"
+
+__plugin_meta__ = PluginMetadata(
+    name="Bison",
+    description="通用订阅推送插件",
+    usage=__usage__,
+    extra={
+        "version": __help__version__,
+    },
+)
 
 __all__ = [
     "admin_page",

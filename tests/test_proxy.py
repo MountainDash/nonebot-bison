7aed36ca87fe12f1fe5811b26843422d25a4d920
hemengyang <hmy0119@gmail.com>
2023-03-02 14:39:22 +0800

    :construction: reload http module to test proxy

--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -1,5 +1,7 @@
-import pytest
+import importlib
+
 from nonebug import App
+from pytest_mock import MockerFixture
 
 
 async def test_without_proxy(app: App):
@@ -11,11 +13,15 @@ async def test_without_proxy(app: App):
     assert "Chrome" in req.headers["User-Agent"]
 
 
-@pytest.mark.parametrize(
-    "nonebug_init", [{"bison_proxy": "http://example.com"}], indirect=True
-)
-async def test_with_proxy(app: App):
-    from nonebot_bison.utils import http_client
+async def test_with_proxy(app: App, mocker: MockerFixture):
+    from nonebot_bison.plugin_config import plugin_config
+
+    mocker.patch.object(plugin_config, "bison_proxy", "http://example.com")
+    from nonebot_bison.utils import http
+
+    importlib.reload(http)
+
+    from nonebot_bison.utils.http import http_client
 
     c = http_client()
     assert c._mounts

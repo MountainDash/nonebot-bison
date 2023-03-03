c3a764581e4a2dcb092cd361c05ba8064725dd1a
felinae98 <731499577@qq.com>
2023-03-03 17:16:15 +0800

    :construction_worker: use pytest-xdist to accelerate test (#186)
    * :construction_worker: use pytest-xdist to accelerate test
    
    * :white_check_mark: mark flaky
    
    * test(mcbbsnews): 删除重复测试，为测试添加flaky
    
    ---------
    
    Co-authored-by: hemengyang <hmy0119@gmail.com>
    Co-authored-by: AzideCupric <rukuy@qq.com>

--- tests/platforms/test_mcbbsnews.py
+++ tests/platforms/test_mcbbsnews.py
@@ -1,5 +1,6 @@
 import pytest
 import respx
+from flaky import flaky
 from httpx import AsyncClient, Response
 from nonebug.app import App
 
@@ -16,93 +17,14 @@ def mcbbsnews(app: App):
 
 @pytest.fixture(scope="module")
 def raw_post_list():
-    return get_json("mcbbsnews/mcbbsnews_raw_post_list.json")
+    return get_json("mcbbsnews/mcbbsnews_raw_post_list_update.json")
 
 
 @pytest.mark.asyncio
 @pytest.mark.render
 @respx.mock
-async def test_javanews_parser(mcbbsnews, raw_post_list):
-    javanews_mock = respx.get("https://www.mcbbs.net/thread-1338607-1-1.html")
-    javanews_mock.mock(
-        return_value=Response(
-            200, text=get_file("mcbbsnews/mock/mcbbsnews_javanews.html")
-        )
-    )
-    raw_post = raw_post_list[3]
-    post = await mcbbsnews.parse(raw_post)
-    assert post.text == "{}\n│\n└由 {} 发表".format(raw_post["title"], raw_post["author"])
-    assert post.target_name == raw_post["category"]
-    assert len(post.pics) == 1
-
-
-@pytest.mark.asyncio
-@pytest.mark.render
-@respx.mock
-async def test_bedrocknews_parser(mcbbsnews, raw_post_list):
-    bedrocknews_mock = respx.get("https://www.mcbbs.net/thread-1338592-1-1.html")
-    bedrocknews_mock.mock(
-        return_value=Response(
-            200, text=get_file("mcbbsnews/mock/mcbbsnews_bedrocknews.html")
-        )
-    )
-    raw_post = raw_post_list[4]
-    post = await mcbbsnews.parse(raw_post)
-    assert post.text == "{}\n│\n└由 {} 发表".format(raw_post["title"], raw_post["author"])
-    assert post.target_name == raw_post["category"]
-    assert len(post.pics) == 1
-
-
-@pytest.mark.asyncio
-@pytest.mark.render
-@respx.mock
-async def test_bedrock_express_parser(mcbbsnews, raw_post_list):
-    bedrock_express_mock = respx.get("https://www.mcbbs.net/thread-1332424-1-1.html")
-    bedrock_express_mock.mock(
-        return_value=Response(
-            200, text=get_file("mcbbsnews/mock/mcbbsnews_bedrock_express.html")
-        )
-    )
-    raw_post = raw_post_list[13]
-    post = await mcbbsnews.parse(raw_post)
-    assert post.target_name == raw_post["category"]
-    assert post.text == "{}\n│\n└由 {} 发表".format(raw_post["title"], raw_post["author"])
-
-
-@pytest.mark.asyncio
-@pytest.mark.render
-@respx.mock
-async def test_java_express_parser(mcbbsnews, raw_post_list):
-    java_express_mock = respx.get("https://www.mcbbs.net/thread-1340080-1-1.html")
-    java_express_mock.mock(
-        return_value=Response(
-            200, text=get_file("mcbbsnews/mock/mcbbsnews_java_express.html")
-        )
-    )
-    raw_post = raw_post_list[0]
-    post = await mcbbsnews.parse(raw_post)
-    assert post.target_name == raw_post["category"]
-    assert post.text == "{}\n│\n└由 {} 发表".format(raw_post["title"], raw_post["author"])
-
-
-@pytest.mark.asyncio
-@pytest.mark.render
-@respx.mock
-async def test_merch_parser(mcbbsnews, raw_post_list):
-    mc_merch_mock = respx.get("https://www.mcbbs.net/thread-1342236-1-1.html")
-    mc_merch_mock.mock(
-        return_value=Response(200, text=get_file("mcbbsnews/mock/mcbbsnews_merch.html"))
-    )
-    raw_post = raw_post_list[26]
-    post = await mcbbsnews.parse(raw_post_list[26])
-    assert post.target_name == raw_post["category"]
-    assert post.text == "{}\n│\n└由 {} 发表".format(raw_post["title"], raw_post["author"])
-
-
-@pytest.mark.asyncio
-@pytest.mark.render
-@respx.mock
-async def test_fetch_new(mcbbsnews, dummy_user_subinfo):
+@flaky(max_runs=3, min_passes=1)
+async def test_fetch_new(mcbbsnews, dummy_user_subinfo, raw_post_list):
     news_router = respx.get("https://www.mcbbs.net/forum-news-1.html")
     news_router.mock(
         return_value=Response(
@@ -127,15 +49,18 @@ async def test_fetch_new(mcbbsnews, dummy_user_subinfo):
     res = await mcbbsnews.fetch_new_post(target, [dummy_user_subinfo])
     assert news_router.called
     post = res[0][1][0]
+    raw_post = raw_post_list[0]
     assert post.target_type == "MCBBS幻翼块讯"
-    assert post.text == "Minecraft Java版 1.19-pre1 发布\n│\n└由 希铁石z 发表"
-    assert post.url == "https://www.mcbbs.net/thread-1340927-1-1.html"
-    assert post.target_name == "Java版资讯"
+    assert post.text == "{}\n│\n└由 {} 发表".format(raw_post["title"], raw_post["author"])
+    assert post.url == "https://www.mcbbs.net/{}".format(raw_post["url"])
+    assert post.target_name == raw_post["category"]
+    assert len(post.pics) == 1
 
 
 @pytest.mark.asyncio
 @pytest.mark.render
 @respx.mock
+@flaky(max_runs=3, min_passes=1)
 async def test_news_render(mcbbsnews, dummy_user_subinfo):
     new_post = respx.get("https://www.mcbbs.net/thread-1340927-1-1.html")
     new_post.mock(

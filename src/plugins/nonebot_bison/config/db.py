fdc58a8134ec0af46b64d95ae2d2e73b239691e6
Azide <rukuy@qq.com>
2023-03-05 02:49:25 +0800

    feat: 实现导出存储的订阅信息的功能

--- src/plugins/nonebot_bison/config/db.py
+++ src/plugins/nonebot_bison/config/db.py
@@ -1,8 +1,14 @@
+import json
+import time
+from pathlib import Path
+
 from nonebot.log import logger
 from nonebot_plugin_datastore.db import get_engine
+from objtyping import to_primitive
 from sqlalchemy.ext.asyncio.session import AsyncSession
 
 from .config_legacy import ConfigContent, config, drop
+from .db_config import config as db_config
 from .db_model import Subscribe, Target, User
 
 
@@ -68,3 +74,55 @@ async def data_migrate():
             await sess.commit()
             drop()
             logger.info("migrate success")
+
+
+async def subscribes_export():
+    """bison订阅信息导出函数，生成可以导入另一个bison的订阅记录文件"""
+
+    # 将数据库查询记录转换为python数据类型，并排除主键和外键
+    raw_subs = to_primitive(
+        db_config.list_subs_with_all_info(), ignores=["id", "target_id", "user_id"]
+    )
+    assert isinstance(raw_subs, list)
+
+    formatted_subs = []
+    users = set()
+
+    for sub in raw_subs:
+        assert isinstance(sub, dict)
+        user = sub["user"]
+
+        if user["uid"] in users:
+            # 已有该user，追加记录到该user下的subs
+            for item in formatted_subs:
+
+                if item["user"] == user:
+                    new_sub = sub.copy()
+                    new_sub.pop("user")
+                    item["subs"].append(new_sub)
+
+                    break
+
+        else:
+            # 新的user，新建该user的订阅信息
+            users.add(user["uid"])
+            subs = []
+
+            new_sub = sub.copy()
+            new_sub.pop("user")
+            subs.append(new_sub)
+
+            formatted_subs.append({"user": user, "subs": subs})
+
+    export_path = Path.cwd() / "data"
+    try:
+        assert export_path.exists()
+    except AssertionError:
+        export_path.mkdir(666)
+    finally:
+        with open(
+            export_path / f"subscribes_export_{int(time.time())}.json",
+            "w",
+            encoding="utf-8",
+        ) as f:
+            json.dump(formatted_subs, f, ensure_ascii=False, indent=4)

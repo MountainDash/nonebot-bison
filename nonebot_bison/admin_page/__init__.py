87dbfeb5bbd6178c00c2889dc5e2befe4709cf12
Azide <rukuy@qq.com>
2023-04-11 13:54:52 +0800

    :white_check_mark: 添加测试以提高覆盖率

--- nonebot_bison/admin_page/__init__.py
+++ nonebot_bison/admin_page/__init__.py
@@ -66,7 +66,7 @@ def init_fastapi():
         f"Nonebot Bison frontend will be running at: "
         f"<b><u>http://{host}:{port}/bison</u></b>"
     )
-    logger.opt(colors=True).info(f"该页面不能被直接访问，请私聊bot <u><b>后台管理</u></b> 以获取可访问地址")
+    logger.opt(colors=True).info(f"该页面不能被直接访问，请私聊bot <b><u>后台管理</u></b> 以获取可访问地址")
 
 
 def register_get_token_handler():

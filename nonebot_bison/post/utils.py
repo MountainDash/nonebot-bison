f357caac15ebde128bfae5b2f5e7e2a2d5e85f6d
Azide <rukuy@qq.com>
2023-05-19 21:52:05 +0800

    :bento: 添加卡片模板文件

--- nonebot_bison/post/utils.py
+++ nonebot_bison/post/utils.py
@@ -8,22 +8,35 @@ from .types import Card
 card_dir = Path(__file__).parent / "templates" / "card"
 
 
+async def card_template_to_pic(template_name: str, card: Card) -> bytes:
+    require("nonebot_plugin_htmlrender")
+    import jinja2
+    from nonebot_plugin_htmlrender import get_new_page
+
+    template_env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(card_dir),
+        enable_async=True,
+    )
+    template = template_env.get_template(template_name)
+    html = await template.render_async(card=card)
+    pages = {
+        "viewport": {"width": 1000, "height": 3000},
+        "base_url": card_dir.as_uri(),
+    }
+    async with get_new_page(**pages) as page:
+        await page.goto(card_dir.as_uri())
+        await page.set_content(html)
+        await page.wait_for_timeout(1)
+        img_raw = await page.locator("#show").screenshot(
+            type="png",
+        )
+    return img_raw
+
+
 async def card_render(card: Card) -> MessageSegment:
     """将Card类提供的数据导入jinja模板渲染, 并返回图片消息段"""
-    require("nonebot_plugin_htmlrender")
-    from nonebot_plugin_htmlrender import template_to_pic as _template_to_pic
-
     template_name = f"{card.type}.html"
 
-    card_pic = await _template_to_pic(
-        template_path=card_dir.as_posix(),
-        template_name=template_name,
-        templates={"card": card},
-        pages={
-            "viewport": {"width": 1000, "height": 3000},
-            "base_url": card_dir.as_uri(),
-        },
-        wait=1,
-    )
+    card_pic = await card_template_to_pic(template_name, card)
 
     return MessageSegment.image(card_pic)

6743929ca275aeb789b3807684edd8aa80265aac
felinae98 <731499577@qq.com>
2023-06-07 02:49:05 +0800

    :truck: reorganize config_manager

---
+++ nonebot_bison/config_manager/query_sub.py
@@ -0,0 +1,41 @@
+from typing import Type
+
+from nonebot.matcher import Matcher
+from nonebot.typing import T_State
+from nonebot_plugin_saa import MessageFactory, PlatformTarget
+
+from ..config import config
+from ..platform import platform_manager
+from ..types import Category
+from ..utils import parse_text
+from .utils import ensure_user_info
+
+
+def do_query_sub(query_sub: Type[Matcher]):
+    query_sub.handle()(ensure_user_info(query_sub))
+
+    @query_sub.handle()
+    async def _(state: T_State):
+        user_info = state["target_user_info"]
+        assert isinstance(user_info, PlatformTarget)
+        sub_list = await config.list_subscribe(user_info)
+        res = "订阅的帐号为：\n"
+        for sub in sub_list:
+            res += "{} {} {}".format(
+                # sub["target_type"], sub["target_name"], sub["target"]
+                sub.target.platform_name,
+                sub.target.target_name,
+                sub.target.target,
+            )
+            platform = platform_manager[sub.target.platform_name]
+            if platform.categories:
+                res += " [{}]".format(
+                    ", ".join(
+                        map(lambda x: platform.categories[Category(x)], sub.categories)
+                    )
+                )
+            if platform.enable_tag:
+                res += " {}".format(", ".join(sub.tags))
+            res += "\n"
+        await MessageFactory(await parse_text(res)).send()
+        await query_sub.finish()

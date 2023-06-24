95f527f2142fea6c3cbe055db9792daf00c64da6
Azide <rukuy@qq.com>
2023-06-24 17:57:43 +0800

    :arrow_up: 支持nb2.0.0并更新metadata

--- nonebot_bison/__init__.py
+++ nonebot_bison/__init__.py
@@ -18,7 +18,7 @@ from . import (
     types,
     utils,
 )
-from .plugin_config import plugin_config
+from .plugin_config import PlugConfig, plugin_config
 
 __help__version__ = "0.7.3"
 nonebot_plugin_saa.enable_auto_select_bot()
@@ -26,13 +26,17 @@ nonebot_plugin_saa.enable_auto_select_bot()
 __help__plugin__name__ = "nonebot_bison"
 __usage__ = f"本bot可以提供b站、微博等社交媒体的消息订阅，详情请查看本bot文档，或者{'at本bot' if plugin_config.bison_to_me else '' }发送“添加订阅”订阅第一个帐号，发送“查询订阅”或“删除订阅”管理订阅"
 
+__supported_adapters__ = nonebot_plugin_saa.__plugin_meta__.supported_adapters
+
 __plugin_meta__ = PluginMetadata(
     name="Bison",
     description="通用订阅推送插件",
     usage=__usage__,
-    extra={
-        "version": __help__version__,
-    },
+    type="application",
+    homepage="https://github.com/felinae98/nonebot-bison",
+    config=PlugConfig,
+    supported_adapters=__supported_adapters__,
+    extra={"version": __help__version__, "docs": "https://nonebot-bison.netlify.app/"},
 )
 
 __all__ = [

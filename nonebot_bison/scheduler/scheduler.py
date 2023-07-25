c94b4c4d78587306038a847c6cde7c6f0b63c24f
felinae98 <731499577@qq.com>
2023-07-25 21:31:12 +0800

    :construction: support batch in scheduler

--- nonebot_bison/scheduler/scheduler.py
+++ nonebot_bison/scheduler/scheduler.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from collections import defaultdict
 
 from nonebot.log import logger
 from nonebot_plugin_apscheduler import scheduler
@@ -16,15 +17,18 @@ class Schedulable:
     platform_name: str
     target: Target
     current_weight: int
+    use_batch: bool = False
 
 
 class Scheduler:
-    schedulable_list: list[Schedulable]
+    schedulable_list: list[Schedulable]  # for load weigth from db
+    batch_api_target_cache: dict[str, dict[Target, list[Target]]]  # platform_name -> (target -> [target])
+    batch_platform_name_targets_cache: dict[str, list[Target]]
 
     def __init__(
         self,
         scheduler_config: type[SchedulerConfig],
-        schedulables: list[tuple[str, Target]],
+        schedulables: list[tuple[str, Target, bool]],  # [(platform_name, target, use_batch)]
         platform_name_list: list[str],
     ):
         self.name = scheduler_config.name
@@ -33,9 +37,17 @@ class Scheduler:
             raise RuntimeError(f"{self.name} not found")
         self.scheduler_config = scheduler_config
         self.scheduler_config_obj = self.scheduler_config()
+
         self.schedulable_list = []
-        for platform_name, target in schedulables:
-            self.schedulable_list.append(Schedulable(platform_name=platform_name, target=target, current_weight=0))
+        self.batch_platform_name_targets_cache: dict[str, list[Target]] = defaultdict(list)
+        for platform_name, target, use_batch in schedulables:
+            if use_batch:
+                self.batch_platform_name_targets_cache[platform_name].append(target)
+            self.schedulable_list.append(
+                Schedulable(platform_name=platform_name, target=target, current_weight=0, use_batch=use_batch)
+            )
+        self._refresh_batch_api_target_cache()
+
         self.platform_name_list = platform_name_list
         self.pre_weight_val = 0  # 轮调度中“本轮”增加权重和的初值
         logger.info(
@@ -47,6 +59,12 @@ class Scheduler:
             self.scheduler_config.schedule_type,
             **self.scheduler_config.schedule_setting,
         )
+
+    def _refresh_batch_api_target_cache(self):
+        self.batch_api_target_cache = defaultdict(dict)
+        for platform_name, targets in self.batch_platform_name_targets_cache.items():
+            for target in targets:
+                self.batch_api_target_cache[platform_name][target] = targets
 
     async def get_next_schedulable(self) -> Schedulable | None:
         if not self.schedulable_list:
@@ -69,14 +87,24 @@ class Scheduler:
         if not (schedulable := await self.get_next_schedulable()):
             return
         logger.trace(f"scheduler {self.name} fetching next target: [{schedulable.platform_name}]{schedulable.target}")
-        send_userinfo_list = await config.get_platform_target_subscribers(schedulable.platform_name, schedulable.target)
 
         client = await self.scheduler_config_obj.get_client(schedulable.target)
         context.register_to_client(client)
 
         try:
             platform_obj = platform_manager[schedulable.platform_name](context, client)
-            to_send = await platform_obj.do_fetch_new_post(schedulable.target, send_userinfo_list)
+            if schedulable.use_batch:
+                batch_targets = self.batch_api_target_cache[schedulable.platform_name][schedulable.target]
+                target_user_info_list = []
+                for batch_target in batch_targets:
+                    userinfo = await config.get_platform_target_subscribers(schedulable.platform_name, batch_target)
+                    target_user_info_list.append((batch_target, userinfo))
+                to_send = await platform_obj.do_batch_fetch_new_post(target_user_info_list)
+            else:
+                send_userinfo_list = await config.get_platform_target_subscribers(
+                    schedulable.platform_name, schedulable.target
+                )
+                to_send = await platform_obj.do_fetch_new_post(schedulable.target, send_userinfo_list)
         except Exception as err:
             records = context.gen_req_records()
             for record in records:
@@ -101,9 +129,18 @@ class Scheduler:
     def insert_new_schedulable(self, platform_name: str, target: Target):
         self.pre_weight_val += 1000
         self.schedulable_list.append(Schedulable(platform_name, target, 1000))
+
+        if platform_manager[platform_name].use_batch:
+            self.batch_platform_name_targets_cache[platform_name].append(target)
+            self._refresh_batch_api_target_cache()
+
         logger.info(f"insert [{platform_name}]{target} to Schduler({self.scheduler_config.name})")
 
     def delete_schedulable(self, platform_name, target: Target):
+        if platform_manager[platform_name].use_batch:
+            self.batch_platform_name_targets_cache[platform_name].remove(target)
+            self._refresh_batch_api_target_cache()
+
         if not self.schedulable_list:
             return
         to_find_idx = None
@@ -114,4 +151,3 @@ class Scheduler:
         if to_find_idx is not None:
             deleted_schdulable = self.schedulable_list.pop(to_find_idx)
             self.pre_weight_val -= deleted_schdulable.current_weight
-        return

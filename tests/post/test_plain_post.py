c38c4c3a6366a371a707e927230fcb8356b433f1
UKMeng <ukmengse@gmail.com>
2023-07-26 15:45:17 +0800

    🧪 test(post): 新增plain_post测试用例

---
+++ tests/post/test_plain_post.py
@@ -0,0 +1,16 @@
+import pytest
+from nonebug import App
+
+
+@pytest.mark.asyncio
+async def test_text_to_pic(app: App):
+    from nonebot_bison.post.plain_post import PlainPost
+
+    post = PlainPost(
+        platform="rss",
+        text="Nonebot Bison 通用的平台消息推送工具，励志做全泰拉骑自行车最快的信使",
+        url="https://nonebot-bison.netlify.app/",
+        target_name="rss-bison",
+    )
+    messages = await post._text_to_img()
+    assert len(messages) == 2

734c06247965509d0dad345fd44247bb18bd9a9b
Azide <rukuy@qq.com>
2024-02-13 16:03:43 +0800

    :white_check_mark: 补充转发测试

--- tests/post/test_generate.py
+++ tests/post/test_generate.py
@@ -103,6 +103,61 @@ Vero hendrerit vero diam et lorem blandit ex diam ex...
 - nickname: 'Mock'
 - description: 'Labore amet ut invidunt dolor consectetuer ipsum sadipscing sed minim diam rebum justo tincidunt.'
 """  # noqa: E501
+    post2 = Post(
+        mock_platform,
+        "Rebum delenit iusto augue in rebum sanctus diam stet clita voluptua amet tempor sea in.\n"
+        "Vel ullamcorper dolore clita eos amet tempor velit amet in.\n"
+        "Vero hendrerit vero diam et lorem blandit ex diam ex amet.\n"
+        "Voluptua et sed diam erat et diam lorem lorem no euismod sadipscing rebum feugiat est elitr autem.\n",
+        title="Ipsum consectetuer voluptua eirmod aliquyam dolore eu volutpat ipsum ipsum eirmod nulla.",
+        images=[
+            b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x06\x00\x00\x00\x1f\x15\xc4\x89",
+            b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x06\x00\x00\x00\x1f\x15\xc4\x89",
+        ],
+        timestamp=1612345678,
+        url="http://t.tt/diam-duo-autem-sed-sadipscing-invidunt",
+        avatar=(
+            b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x06\x00\x00\x00\x1f\x15\xc4\x89"
+        ),
+        nickname="Mock2",
+        description="Labore amet ut invidunt dolor consectetuer ipsum sadipscing sed minim diam rebum justo tincidunt.",
+        repost=post,
+    )
+    assert str(post2) == rf"""## Post: {id(post2):X} ##
+
+Rebum delenit iusto augue in rebum sanctus diam stet clita voluptua amet tempor sea in.
+Vel ullamcorper dolore clita eos amet tempor velit amet in.
+Vero hendrerit vero diam et lorem blandit ex diam ex...
+
+来源: <Platform mock-platform>
+附加信息:
+- compress: False
+- title: 'Ipsum consectetuer voluptua eirmod aliquyam dolore eu volutpat ipsum ipsum eirmod nulla.'
+- images: [b'\x89PNG\r\n...f\x15\xc4\x89', b'\x89PNG\r\n...f\x15\xc4\x89']
+- timestamp: 1612345678
+- url: 'http://t.tt/diam-duo-autem-sed-sadipscing-invidunt'
+- avatar: b'\x89PNG\r\n...f\x15\xc4\x89'
+- nickname: 'Mock2'
+- description: 'Labore amet ut invidunt dolor consectetuer ipsum sadipscing sed minim diam rebum justo tincidunt.'
+
+转发:
+## Post: {id(post):X} ##
+
+Rebum delenit iusto augue in rebum sanctus diam stet clita voluptua amet tempor sea in.
+Vel ullamcorper dolore clita eos amet tempor velit amet in.
+Vero hendrerit vero diam et lorem blandit ex diam ex...
+
+来源: <Platform mock-platform>
+附加信息:
+- compress: False
+- title: 'Ipsum consectetuer voluptua eirmod aliquyam dolore eu volutpat ipsum ipsum eirmod nulla.'
+- images: [b'\x89PNG\r\n...f\x15\xc4\x89', b'\x89PNG\r\n...f\x15\xc4\x89']
+- timestamp: 1612345678
+- url: 'http://t.tt/diam-duo-autem-sed-sadipscing-invidunt'
+- avatar: b'\x89PNG\r\n...f\x15\xc4\x89'
+- nickname: 'Mock'
+- description: 'Labore amet ut invidunt dolor consectetuer ipsum sadipscing sed minim diam rebum justo tincidunt.'
+"""
 
 
 @pytest.mark.asyncio

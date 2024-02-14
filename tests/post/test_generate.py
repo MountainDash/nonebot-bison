4fd06025c88bb4b1acd731bd2fb861865d46740e
AzideCupric <rukuy@qq.com>
2024-02-15 00:14:21 +0800

    :sparkles: ✨ 优化 Post 的 __str__ 输出 (#476)
    * :sparkles: 优化Post类的__str__
    
    * :white_check_mark: 补充转发测试

--- tests/post/test_generate.py
+++ tests/post/test_generate.py
@@ -24,8 +24,8 @@ def mock_platform(app: App):
     from nonebot_bison.platform.platform import NewMessage
 
     class MockPlatform(NewMessage):
-        platform_name = "mock_platform"
-        name = "Mock Platform"
+        platform_name = "mock-platform"
+        name = "Mock-Platform"
         enabled = True
         is_common = True
         schedule_interval = 10
@@ -37,7 +37,7 @@ def mock_platform(app: App):
 
         @classmethod
         async def get_target_name(cls, client, _: "Target"):
-            return "MockPlatform"
+            return "MockPlatformTarget"
 
         def get_id(self, post: "RawPost") -> Any:
             return post["id"]
@@ -50,7 +50,7 @@ def mock_platform(app: App):
                 self,
                 raw_post["text"],
                 url="http://t.tt/" + str(self.get_id(raw_post)),
-                nickname="Mock",
+                nickname="MockNick",
             )
 
         @classmethod
@@ -62,6 +62,102 @@ def mock_platform(app: App):
                 return raw_post_list_2
 
     return MockPlatform
+
+
+async def test_display(mock_platform):
+    from nonebot_bison.post import Post
+
+    post = Post(
+        mock_platform,
+        "Rebum delenit iusto augue in rebum sanctus diam stet clita voluptua amet tempor sea in.\n"
+        "Vel ullamcorper dolore clita eos amet tempor velit amet in.\n"
+        "Vero hendrerit vero diam et lorem blandit ex diam ex amet.\n"
+        "Voluptua et sed diam erat et diam lorem lorem no euismod sadipscing rebum feugiat est elitr autem.\n",
+        title="Ipsum consectetuer voluptua eirmod aliquyam dolore eu volutpat ipsum ipsum eirmod nulla.",
+        images=[
+            b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x06\x00\x00\x00\x1f\x15\xc4\x89",
+            b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x06\x00\x00\x00\x1f\x15\xc4\x89",
+        ],
+        timestamp=1612345678,
+        url="http://t.tt/diam-duo-autem-sed-sadipscing-invidunt",
+        avatar=(
+            b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x06\x00\x00\x00\x1f\x15\xc4\x89"
+        ),
+        nickname="Mock",
+        description="Labore amet ut invidunt dolor consectetuer ipsum sadipscing sed minim diam rebum justo tincidunt.",
+    )
+    assert str(post) == rf"""## Post: {id(post):X} ##
+
+Rebum delenit iusto augue in rebum sanctus diam stet clita voluptua amet tempor sea in.
+Vel ullamcorper dolore clita eos amet tempor velit amet in.
+Vero hendrerit vero diam et lorem blandit ex diam ex...
+
+来源: <Platform mock-platform>
+附加信息:
+- compress: False
+- title: 'Ipsum consectetuer voluptua eirmod aliquyam dolore eu volutpat ipsum ipsum eirmod nulla.'
+- images: [b'\x89PNG\r\n...f\x15\xc4\x89', b'\x89PNG\r\n...f\x15\xc4\x89']
+- timestamp: 1612345678
+- url: 'http://t.tt/diam-duo-autem-sed-sadipscing-invidunt'
+- avatar: b'\x89PNG\r\n...f\x15\xc4\x89'
+- nickname: 'Mock'
+- description: 'Labore amet ut invidunt dolor consectetuer ipsum sadipscing sed minim diam rebum justo tincidunt.'
+"""  # noqa: E501
+    post2 = Post(
+        mock_platform,
+        "Rebum delenit iusto augue in rebum sanctus diam stet clita voluptua amet tempor sea in.\n"
+        "Vel ullamcorper dolore clita eos amet tempor velit amet in.\n"
+        "Vero hendrerit vero diam et lorem blandit ex diam ex amet.\n"
+        "Voluptua et sed diam erat et diam lorem lorem no euismod sadipscing rebum feugiat est elitr autem.\n",
+        title="Ipsum consectetuer voluptua eirmod aliquyam dolore eu volutpat ipsum ipsum eirmod nulla.",
+        images=[
+            b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x06\x00\x00\x00\x1f\x15\xc4\x89",
+            b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x06\x00\x00\x00\x1f\x15\xc4\x89",
+        ],
+        timestamp=1612345678,
+        url="http://t.tt/diam-duo-autem-sed-sadipscing-invidunt",
+        avatar=(
+            b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x06\x00\x00\x00\x1f\x15\xc4\x89"
+        ),
+        nickname="Mock2",
+        description="Labore amet ut invidunt dolor consectetuer ipsum sadipscing sed minim diam rebum justo tincidunt.",
+        repost=post,
+    )
+    assert str(post2) == rf"""## Post: {id(post2):X} ##
+
+Rebum delenit iusto augue in rebum sanctus diam stet clita voluptua amet tempor sea in.
+Vel ullamcorper dolore clita eos amet tempor velit amet in.
+Vero hendrerit vero diam et lorem blandit ex diam ex...
+
+来源: <Platform mock-platform>
+附加信息:
+- compress: False
+- title: 'Ipsum consectetuer voluptua eirmod aliquyam dolore eu volutpat ipsum ipsum eirmod nulla.'
+- images: [b'\x89PNG\r\n...f\x15\xc4\x89', b'\x89PNG\r\n...f\x15\xc4\x89']
+- timestamp: 1612345678
+- url: 'http://t.tt/diam-duo-autem-sed-sadipscing-invidunt'
+- avatar: b'\x89PNG\r\n...f\x15\xc4\x89'
+- nickname: 'Mock2'
+- description: 'Labore amet ut invidunt dolor consectetuer ipsum sadipscing sed minim diam rebum justo tincidunt.'
+
+转发:
+## Post: {id(post):X} ##
+
+Rebum delenit iusto augue in rebum sanctus diam stet clita voluptua amet tempor sea in.
+Vel ullamcorper dolore clita eos amet tempor velit amet in.
+Vero hendrerit vero diam et lorem blandit ex diam ex...
+
+来源: <Platform mock-platform>
+附加信息:
+- compress: False
+- title: 'Ipsum consectetuer voluptua eirmod aliquyam dolore eu volutpat ipsum ipsum eirmod nulla.'
+- images: [b'\x89PNG\r\n...f\x15\xc4\x89', b'\x89PNG\r\n...f\x15\xc4\x89']
+- timestamp: 1612345678
+- url: 'http://t.tt/diam-duo-autem-sed-sadipscing-invidunt'
+- avatar: b'\x89PNG\r\n...f\x15\xc4\x89'
+- nickname: 'Mock'
+- description: 'Labore amet ut invidunt dolor consectetuer ipsum sadipscing sed minim diam rebum justo tincidunt.'
+"""
 
 
 @pytest.mark.asyncio
@@ -77,7 +173,7 @@ async def test_generate_msg(mock_platform):
     res = await post.generate()
     assert len(res) == 1
     assert isinstance(res[0], Text)
-    assert str(res[0]) == "p1\n来源: Mock Platform Mock\n详情: http://t.tt/1"
+    assert str(res[0]) == "p1\n来源: Mock-Platform MockNick\n详情: http://t.tt/1"
 
     post.platform.default_theme = "ht2i"
     assert post.get_config_theme() is None

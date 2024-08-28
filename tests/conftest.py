8cf0c3f482dbdd505fa645dcc7251035fc2e8f5c
uy_sun <hmy0119@gmail.com>
2024-08-28 12:53:19 +0800

    :white_check_mark: fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,6 +18,7 @@ def pytest_configure(config: pytest.Config) -> None:
         "superusers": {"10001"},
         "command_start": {""},
         "log_level": "TRACE",
+        "bison_use_browser": True,
     }
 
 

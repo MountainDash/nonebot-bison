e6c45e5b1b5f8cf4f10bd3084f5ca4ad13ebf4d7
suyiiyii <suyiiyii@gmail.com>
2024-10-28 23:56:10 +0800

    :recycle: 为 row2dict 添加类型注解

--- nonebot_bison/config/subs_io/utils.py
+++ nonebot_bison/config/subs_io/utils.py
@@ -1,10 +1,13 @@
+from ..db_model import Model
+
+
 class NBESFVerMatchErr(Exception): ...
 
 
 class NBESFParseErr(Exception): ...
 
 
-def row2dict(row):
+def row2dict(row: Model) -> dict:
     d = {}
     for column in row.__table__.columns:
         d[column.name] = str(getattr(row, column.name))

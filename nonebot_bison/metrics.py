8cafe38bac106eeb29570eed34c5ce4dc52e32b7
suyiiyii <suyiiyii@gmail.com>
2024-12-12 13:08:27 +0800

    :sparkles: 使用 label 简化 request_counter

--- nonebot_bison/metrics.py
+++ nonebot_bison/metrics.py
@@ -3,12 +3,13 @@ from starlette.responses import Response
 from prometheus_client import CONTENT_TYPE_LATEST, Counter, generate_latest
 
 # Request counter
-request_counter = Counter("bison_request_counter", "The number of requests")
-# Success counter
-success_counter = Counter("bison_success_counter", "The number of successful requests")
+request_counter = Counter(
+    "bison_request_counter", "The number of requests", ["site_name", "platform_name", "target", "success"]
+)
 
 # Sent counter
-sent_counter = Counter("bison_sent_counter", "The number of sent messages")
+sent_counter = Counter("bison_sent_counter", "The number of sent messages", ["site_name", "platform_name", "target"])
+
 metrics_router = APIRouter(prefix="/api/metrics", tags=["metrics"])
 
 
